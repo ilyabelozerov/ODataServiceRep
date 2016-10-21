d03da46a4fd1cf5b0655df9329a88b7b95d36aa8
Ilya Belozerov <belozerov.ilya@gmail.com>
2016-10-21 20:44:51 +0200

    Products Entity - basic OData methods

--- ODataService/App_Start/WebApiConfig.cs
+++ ODataService/App_Start/WebApiConfig.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
+using ODataService.Models;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+
 namespace ODataService
 {
     public static class WebApiConfig
@@ -10,15 +14,12 @@ namespace ODataService
         public static void Register(HttpConfiguration config)
         {
             // Конфигурация и службы веб-API
-
-            // Маршруты веб-API
-            config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
         }
     }
 }
