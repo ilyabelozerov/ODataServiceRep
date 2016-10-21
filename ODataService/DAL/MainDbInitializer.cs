893f264e7d7f720fa57bffeabdff6f8f60043fbb
Ilya Belozerov <belozerov.ilya@gmail.com>
2016-10-21 22:22:12 +0200

    with Seeder

--- ODataService/DAL/MainDbInitializer.cs
+++ ODataService/DAL/MainDbInitializer.cs
@@ -11,6 +11,12 @@ namespace ODataService.DAL
     {
         protected override void Seed(MainDbContext context)
         {
+            context.Products.Add(new Product { Id = 1, Name = "IPhone", Category = "Phones", Price = 900 });
+            context.Products.Add(new Product { Id = 2, Name = "Samsung", Category = "Phones", Price = 600 });
+            context.Products.Add(new Product { Id = 3, Name = "LG", Category = "Phones", Price = 300 });
+            context.Products.Add(new Product { Id = 4, Name = "Mac Book Pro", Category = "Laptops", Price = 1900 });
+            context.Products.Add(new Product { Id = 5, Name = "Mac Book Air", Category = "Laptops", Price = 1300 });
+
             base.Seed(context);
         }
     }
