72242f4b86269d08efe553713a77e03effc1a161
Ilya Belozerov <belozerov.ilya@gmail.com>
2016-10-29 09:08:37 +0200

    Batch requests handler

--- ODataService/App_Start/WebApiConfig.cs
+++ ODataService/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web.Http;
 
 using ODataService.Models;
+using System.Web.OData.Batch;
 using System.Web.OData.Builder;
 using System.Web.OData.Extensions;
 using System.Web.Http.Cors;
@@ -20,13 +21,24 @@ namespace ODataService
 
             config.Select().Expand().Filter().OrderBy().MaxTop(null).Count();
 
+            // Подключаем обработчик Batch-запросов
+            ODataBatchHandler odataBatchHandler =
+                new DefaultODataBatchHandler(GlobalConfiguration.DefaultServer);
+            odataBatchHandler.MessageQuotas.MaxOperationsPerChangeset = 10;
+            odataBatchHandler.MessageQuotas.MaxPartsPerBatch = 10;
+
             // Конфигурация и службы веб-API
             ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.Namespace = "belozerov";
+            builder.ContainerName = "SqliteContext";
             builder.EntitySet<Product>("Products");
+
             config.MapODataServiceRoute(
                 routeName: "ODataRoute",
                 routePrefix: null,
-                model: builder.GetEdmModel());
+                model: builder.GetEdmModel(),
+                batchHandler: odataBatchHandler
+                );
         }
     }
 }
